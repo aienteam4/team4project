618963afc6650fa77a0b9e516f5176c50b2729f0
tony <tony@iii.org.tw>
2018-09-19 18:48:19 +0800

    進度太慢

--- virtual/team4/firstproject/settings.py
+++ virtual/team4/firstproject/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '&(g!q)iv=b=4pqf3vw7_@u+4tdw#(n3e$lsngpud0enz%gyv7@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
