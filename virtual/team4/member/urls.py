e76756e9c382dfbc5bcbab1048597de6e4e86275
aienteam4 <ken@gmail.com>
2018-10-03 16:04:18 +0800

    kenhsu header&scroll bar

--- virtual/team4/member/urls.py
+++ virtual/team4/member/urls.py
@@ -6,31 +6,14 @@ app_name = "member"
 
 urlpatterns = [
     #http://localhost:8000/member
-    path('',views.index,name="index"),  #urlName
-    #views表示views.py的module
-    #index表示views.py下的function
-    #http://localhost:8000/member/login
+    path('',views.index,name="index"),  
     path('login/',views.login,name="login"),
-
-    #http://localhost:8000/member/logout
     path('logout/',views.logout,name="logout"),
-
-    
-    #http://localhost:8000/member/test
-    path('test/',views.testencoding,name="testencoding"),
-
-
-    #http://localhost:8000/member/register
+    # path('test/',views.testencoding,name="testencoding"),
     path('register/',views.register,name="register"),
-
-    #http://localhost:8000/member/delete/1
     path('delete/<int:id>',views.delete,name="delete"),
-
-    #http://localhost:8000/member/update/1
     path('update/<int:id>', views.update, name="update"),
-
-     #http://localhost:8000/member/show
     path('show/', views.show),
      #http://localhost:8000/member/check/Tom
-    path('check/<str:name>',views.checkname, name="checkname"),
+    # path('check/<str:name>',views.checkname, name="checkname"),
 ]
